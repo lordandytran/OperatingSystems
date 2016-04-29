6b2d1357668b10b599f3377c8bc4ce05917917af
Andy Tran <lordandytran@users.noreply.github.com>
2016-04-29 09:03:33 -0700

    yo

--- PCB.h
+++ PCB.h
@@ -1,13 +1,13 @@
 #ifndef PCB_H
 #define PCB_H
 
-#include <time.h>
+//#include <time.h>
 
 #define MIN_PRIORITY 1
 #define MAX_PRIORITY 30
 
-#define MAX_PC_VAL 2345
-#define IO_TRAPS 4
+//#define MAX_PC_VAL 2345
+//#define IO_TRAPS 4
 
 enum state_type { new_, ready, running, interrupted, waiting, halted };
 
@@ -17,14 +17,14 @@ typedef struct pcb {
 	enum state_type state;
 	unsigned short priority;
 	unsigned long pc;
-	unsigned long sw;
-	unsigned int max_pc;
-	time_t creation;
-	time_t termination;
-	unsigned int terminate;
-	unsigned int term_count;
-	unsigned int io_1_traps[IO_TRAPS];
-	unsigned int io_2_traps[IO_TRAPS];
+	//unsigned long sw;
+	//unsigned int max_pc;
+	//time_t creation;
+	//time_t termination;
+	//unsigned int terminate;
+	//unsigned int term_count;
+	//unsigned int io_1_traps[IO_TRAPS];
+	//unsigned int io_2_traps[IO_TRAPS];
 
 } PCB;
 typedef PCB * PCB_p;
