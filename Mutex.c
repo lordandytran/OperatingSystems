a782100c78e694c58c296ffd8ab7d01c878fafcf
Andy Tran <thematulaaklives@gmail.com>
2016-05-28 12:37:24 -0700

    sup

--- Mutex.c
+++ Mutex.c
@@ -5,32 +5,61 @@
 
 #include <stdlib.h>
 
-Mutex_p mutex_constructor(int ident) {
+Mutex_p Mutex_constructor(unsigned long ident) {
 	Mutex_p mutex = (Mutex_p)malloc(sizeof(Mutex));
-	mutex->ident = ident;
-	mutex->pcb = NULL;
+	mutex->ID = ident;
+	mutex->pcbID = 0;
+	mutex->locked = FALSE;
 	mutex->wait = FIFOq_construct();
-	mutex->lock = TRUE;
 	return mutex;
 }
 
+//TRUE if Successful. FALSE if lock failed.
 int Mutex_lock(Mutex_p mut, PCB_p pcb) {
 	int* error = 0;
-	if (mut->pcb == NULL) {
-		mut->pcb = pcb;
-		mut->lock = TRUE;
+	if (mut->pcbID == 0 || mut->pcbID == pcb->pid) {
+		mut->pcbID = pcb->pid;
+		mut->locked = TRUE;
 		return TRUE;
 	}
 	else {
-		FIFOq_enqueue(mut->wait, pcb);
+		FIFOq_enqueue(mut->wait, pcb, error);
 		return FALSE;
 	}
 }
 
+//TRUE if Successful. FALSE if unlock failed.
 int Mutex_unlock(Mutex_p mut, PCB_p pcb) {
-
+	int* error = 0;
+	if (mut->locked == FALSE)
+		return TRUE;
+	if (pcb->pid == mut->pcbID) {
+		mut->locked = FALSE;
+		return TRUE;
+	}
+	return FALSE;
 }
 
+//replace controller with given and locks. True if successful. False if failed.
 int Mutex_trylock(Mutex_p mut, PCB_p pcb) {
+	if (mut->locked == FALSE) {
+		mut->pcbID = pcb->pid;
+		mut->locked = TRUE;
+		return TRUE;
+	}
+	else
+		return FALSE;
+}
 
+//Force unlocks and replaces controller with next in queue.
+void Mutex_next_Controller(Mutex_p mut) {
+	int* error = 0;
+	mut->pcbID = ((PCB_p)FIFOq_dequeue(mut->wait, error))->pid;
+	mut->locked = FALSE;
+}
+
+int Mutex_Is_Locked(Mutex_p mut) {
+	if (mut->locked)
+		return TRUE;
+	return FALSE;
 }
--- Mutex.h
+++ Mutex.h
@@ -9,18 +9,20 @@
 
 typedef struct mutex_t {
 
-	int ident;
-	int lock;
-	PCB_p pcb;
-	FIFOq_p wait;
+	unsigned long ID; //ID of mutex
+	unsigned long pcbID; //Which PCB has control
+	int locked; //TRUE if locked. FALSE if not
+	FIFOq_p wait; //Queue of PCB's that want control
 
 } Mutex;
 typedef Mutex * Mutex_p;
 
 //prototypes
-Mutex_p mutex_constructor(int);
-int mutex_lock(Mutex_p, PCB_p);
-int mutex_unlock(Mutex_p, PCB_p);
-int mutex_trylock(Mutex_p, PCB_p);
+Mutex_p Mutex_constructor(unsigned long);
+int Mutex_lock(Mutex_p, PCB_p);
+int Mutex_unlock(Mutex_p, PCB_p);
+int Mutex_trylock(Mutex_p, PCB_p);
+void Mutex_next_Controller(Mutex_p, PCB_p);
+int Mutex_Is_Locked(Mutex_p);
 
 #endif
